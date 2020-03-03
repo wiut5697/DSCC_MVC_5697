435c748612b579e77568970212125f4028a8e53e
Nurali Salimov <nsalimov997@gmail.com>
2020-03-03 23:45:54 +0500

    My Second Commit

---
+++ DSCC_CW1_MVC_5697/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DSCC_CW1_MVC_5697.MvcApplication" Language="C#" %>
---
+++ DSCC_CW1_MVC_5697/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DSCC_CW1_MVC_5697.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+    }
+}
